5ea92fc8b1b77447637da4e816238f2c5b5246ad
dheliashaf <dheliashaf23@gmail.com>
2024-12-09 11:38:31 +0700

    fix splash,add model manner

--- app/src/main/java/com/example/tabi/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/tabi/ui/activities/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.tabi.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tabi.R
 
@@ -12,10 +13,12 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        // Delay selama 3 detik sebelum berpindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Intent untuk berpindah ke MainActivity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish()
-        },3000)
+            finish() // Menutup SplashScreenActivity agar tidak kembali ke layar ini
+        }, 3000) // 3000 ms = 3 detik
     }
 }
